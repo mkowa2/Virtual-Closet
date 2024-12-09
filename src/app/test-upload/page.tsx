bfb56a9b090b202031c3642411e210864cc7faa7
Marcin Kowalik <mkowa2@uic.edu>
2024-12-09 00:17:59 -0600

    Image limit

--- src/app/test-upload/page.tsx
+++ src/app/test-upload/page.tsx
@@ -166,7 +166,7 @@ const TestUploadPage = () => {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4">
-      <h1 className="text-2xl font-bold mb-4">Add Clothing Item</h1>
+      <h1 className="text-2xl font-bold mb-4">Add Clothing Item "Less than 4.3 mb images"</h1>
 
       <input
         type="file"
